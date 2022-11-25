f2f566be29c57270f1399373babedf568a9b8fcf
Ruslan425 <92014369+Ruslan425@users.noreply.github.com>
2022-11-25 21:50:38 +0300

    add test

---
+++ src/PostService.js
@@ -0,0 +1,49 @@
+import Post from "./src/Post.js";
+import FileService from "./src/FileService.js";
+
+
+class PostService {
+
+    async create(post, image) {
+        const fileName = FileService.saveImage(image);
+        const createPost = await Post.create({...post, image: fileName});
+        return createPost;
+    };
+
+    async getAll() {
+        const posts = await Post.find();
+        return posts;
+    }
+
+    async getFromId(id) {
+        if (!id) {
+            throw new Error('Dont have ID')
+        }
+        const post = await Post.findById(id)
+        return post
+    }
+
+    async udate(post) {
+        if (!post._id) {
+            throw new Error('Dont have ID')
+        }
+        const updatePost = await Post.findByIdAndUpdate(post._id, post, { new: true })
+        return updatePost;
+    }
+
+    async delete(id) {
+        if (!id) {
+            throw new Error('Dont have ID')
+        }
+        const post = await Post.findByIdAndDelete(id)
+        return post
+    }
+
+    async deleteAll() {
+        const post = await Post.deleteMany()
+        return post
+    }
+
+};
+
+export default new PostService();
